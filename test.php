6413f23bd184dcff4b453b9e9b37c79027b23ee9
Michael Cunningham <mbc95@case.edu>
2018-11-05 16:36:33 -0500

    Changed PHP

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-	//Step1
-	 $db = mysqli_connect('localhost','root','root','recyclepedia')
+	//Connects to the database
+	 $db = mysqli_connect('localhost','root','','recyclepedia')
  	or die('Error connecting to MySQL server.');
 	?>
 
@@ -8,21 +8,33 @@
 	 <head>
 	 </head>
  	<body>
- 		<h1>PHP connect to MySQL</h1>
-
 		<?php
-			//Step2
-			$query = "SELECT * FROM table_name";
-			mysqli_query($db, $query) or die('Error querying database.');
-
-			//Step3
-			$result = mysqli_query($db, $query);
+			//Querys Materials
+			$queryMaterials = "SELECT * FROM Materials";
+			mysqli_query($db, $queryMaterials) or die('Error querying database. ' .  mysqli_error($db));
+			//Querys Policies
+			$queryPolicies = "SELECT * FROM Policy";
+			mysqli_query($db, $queryPolicies) or die('Error querying database. ' .  mysqli_error($db));
+			//Querys Areas
+			$queryAreas = "SELECT * FROM Area";
+			mysqli_query($db, $queryAreas) or die('Error querying database. ' .  mysqli_error($db));
+			
+			//Prints the query to the website
+			$result = mysqli_query($db, $queryMaterials);
 			$row = mysqli_fetch_array($result);
-	
+			$areas = mysqli_query($db, $queryAreas);
+			$area = mysqli_fetch_array($areas);
+
+			//Prints Areas
+			echo 'Welcome to ';
+			echo $area['zipcode'];
+			echo '<br /> <br />';
+			//Prints Materials and Disposal Method
 			while ($row = mysqli_fetch_array($result)) {
-			 echo $row['first_name'] . ' ' . $row['last_name'] . ': ' . $row['email'] . ' ' . $row['city'] .'<br />';
+			 echo $row['material_name'] . ': ' . $row['method_to_break_down'] .'<br />';
 			}
-			//Step 4
+
+			//Closes connection to the database
 			mysqli_close($db);
 		?>
 
