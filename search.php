3a30316754f0c5d961f1f683cadf01f785f0dd61
Michael Cunningham <mbc95@case.edu>
2018-11-14 16:21:50 -0500

    Finished search
    The webpage is unstyled however

--- search.php
+++ search.php
@@ -21,7 +21,7 @@
 			//Prints the query to the website
             //Areas
 			$areas = mysqli_query($db, $queryAreas);
-			$area = mysqli_fetch_array($areas);
+			//$area = mysqli_fetch_array($areas);
 
 			//Prints Area
 			echo '<div class="Greeting">Welcome to ';
@@ -41,7 +41,6 @@
             //$policyid = mysqli_fetch_array($policies);
             while ($policyid = mysqli_fetch_array($policies)) {
                 if ($policyid['policy'] == $area['policy']) {
-                    echo $policyid['policy'];
                     break;
                 }
             }
@@ -53,10 +52,29 @@
             //$recycleid = mysqli_fetch_array($recyclables));
             while ($recycleid = mysqli_fetch_array($recyclables)) {
                 if ($recycleid['recycle_id'] == $policyid['recycle_id']) {
-                    echo $recycleid['recycle_id'];
                     break;
                 }
             }
+        
+            //Querys Landifll
+            $queryLandfill = "Select * FROM Landfill";
+            mysqli_query($db, $queryLandfill) or die('Error querying database. ' . mysqli_error($db));
+            $trash = mysqli_query($db, $queryLandfill);
+            while($trash = mysqli_fetch_array($queryLandfill)) {
+                if ($policyid['landfill_id'] == $trash['landfill_id'])
+                    break;
+            }
+        
+            //Query Waste Center
+            $queryWaste = "Select * FROM Waste_Center";
+            mysqli_query($db, $queryWaste) or die('Error querying database. ' . mysqli_error($db));
+            $waste = mysqli_query($db, $queryWaste);
+            while($waste = mysqli_fetch_array($queryWaste)) {
+                if ($policyid['waste_center_id'] == $waste['waste_center_id'])
+                    break;
+            }
+            
+            
         
             //Querys Materials
 			$queryMaterials = "SELECT * FROM Materials";
@@ -64,11 +82,16 @@
             //Prints corresponding materials
 			$result = mysqli_query($db, $queryMaterials);
 			while($row = mysqli_fetch_array($result)) {
-                if ($row['material_id'] == $recycleid['R1_id']) {
+                //Recyclables
+                if ($row['material_id'] == $recycleid['R1_id'] || $row['material_id'] == $recycleid['R2_id'] || $row['material_id'] == $recycleid['R3_id'] || $row['material_id'] == $recycleid['R4_id'])
                     echo $row['material_name'] . ': ' . $row['method_to_break_down'] . '<br />';
-                    break;
+                //Landfill
+                if ($row['material_id'] == $trash['L1_id'] || $row['material_id'] == $trash['L2_id'] || $row['material_id'] == $trash['L3_id'] || $row['material_id'] == $trash['L4_id'])
+                    echo $row['material_name'] . ': ' . $row['method_to_break_down'] . '<br />';
+                //Waste Center
+                if ($row['material_id'] == $waste['WC1_id'] || $row['material_id'] == $waste['WC2_id'] || $row['material_id'] == $waste['WC3_id'] || $row['material_id'] == $waste['WC4_id'])
+                    echo $row['material_name'] . ': ' . $row['method_to_break_down'] . '<br />';
                 }
-            }
             
             
             //Prints Materials and Disposal Method
